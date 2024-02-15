e597a1ad1b197d05c02d66d0b986ac3038b9e286
EmilieEnne <130250704+EmilieEnne@users.noreply.github.com>
2024-02-15 17:09:44 +0100

    new button added

---
+++ src/components/ButtonGåTilKamp.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Arrow from '../Assets/20240215arrow-right 1.png'
+ 
+type Props = {}
+
+const Button = (prop : Props) => {
+  return (
+    <button className='btn'>
+        <span>Gå til kampange</span>
+        <img src={Arrow} alt="pil" />
+    </button>
+  )
+}
+
+export default Button
