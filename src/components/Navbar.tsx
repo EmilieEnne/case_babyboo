26ac6c5927564c0094d52a77f3c92fa8fe5f2f46
EmilieEnne <130250704+EmilieEnne@users.noreply.github.com>
2024-02-15 17:09:30 +0100

    fixed header og navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,40 @@
 
-type Props = {}
+import Logo from '../Assets/BABYBOO.png';
+import Søg from '../Assets/search 1.png'
+import Profile from './Profile';
+import './NavBar.css'
 
-const Navbar = (props: Props) => {
+
+const Navbar = () => {   
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+        <a className="navbar-brand" href="#">
+            <img 
+            className="logo" 
+            src={Logo} 
+            alt="Babyboo logo" />
+        </a>
+        <div className="valg">
+            <span>SERIER</span>
+            <span>VUGGE</span>
+            <span>BØRNESENGE</span>
+            <span>OPBEVARING</span>
+            <span>HØJSTOL</span>
+            <span>PUSLE</span>
+            <span>TEKSTILER</span>
+            <span>TILBEHØR</span>
+            <span>RESERVEDELE</span>
+        </div>
+        <div className="search">
+            Søg
+            <img 
+            src={Søg} 
+            alt="lup" />
+        </div>
+        <div className="profile">
+            <Profile />
+        </div>
+    </nav>
   )
 }
 
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,7 @@
 import User from '../Assets/user-check 1.png';
 import Globe from '../Assets/Vector (Stroke).png';
 import Arrow from '../Assets/chevron-down 1.png';
+import './Profile.css'
 
 
 type Props = {}
@@ -12,14 +13,13 @@ function Profile({}: Props) {
         <img 
           src={User} 
           alt="user icon"
-          height={'14.25px'}
         />
         <span className='user'>
             Babybob
         </span>
       </div>
       <div className="land">
-        <img src={Globe} alt="vector globe" height={'12px'} />
+        <img src={Globe} alt="vector globe"/>
         <span>DK</span>
         <img src={Arrow} alt="arrow down" />
       </div>  
