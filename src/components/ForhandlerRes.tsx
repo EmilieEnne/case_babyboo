98ec01912b91572ddd981e5523c16a4620ac2568
EmilieEnne <130250704+EmilieEnne@users.noreply.github.com>
2024-02-15 16:12:56 +0100

    Forhandler ressourcer færdig

--- src/components/ForhandlerRes.tsx
+++ src/components/ForhandlerRes.tsx
@@ -2,17 +2,21 @@ import React from 'react'
 import ResCard from './ResCard'
 import ResCard2 from './ResCard2'
 import ResCard3 from './ResCard3' 
+import ResCard4 from './ResCard4'
+import './ForhandlerRes.css'
+
 
 type Props = {}
 
 const ForhandlerRes = (props: Props) => {
   return (
     <div className='forhandler-wrapper'>
-        Dine forhandler ressourcer for <strong>DYLAN puslepude</strong>
+        <p className='forhandler-title'>Dine forhandler ressourcer for <strong>DYLAN puslepude</strong></p>
       <div>
         <ResCard />
         <ResCard2 />
         <ResCard3 />
+        <ResCard4 />
       </div>    
     </div>
   )
