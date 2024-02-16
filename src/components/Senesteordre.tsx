aeaae7019b44597778b679ea787e894b19f00a29
EmilieEnne <130250704+EmilieEnne@users.noreply.github.com>
2024-02-16 15:04:40 +0100

    Styling af seneste ordre komponent

--- src/components/Senesteordre.tsx
+++ src/components/Senesteordre.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import Mappe from '../Assets/folder-history 1.png'
+import './Senesteordre.css'
 
 type Props = {}
 
 const Senesteordre = (props: Props) => {
   return (
-    <div>Senesteordre</div>
+    <div className='ordre-historie'>
+       <img src={Mappe} alt="Mappe vector" />
+        <h2>SENESTE ORDRER</h2>
+        <span><a href="">Se alle</a></span>  
+        <div className="seneste">
+          <ul>
+            <li className='white'>02-02-2022 <strong>#4134534 </strong> <a href=" ">Se ordre</a> <a href="">Genbestil</a></li>
+            <li>02-02-2022 <strong>#4134534 </strong>  <a href=" ">Se ordre</a> <a href="">Genbestil</a></li>
+            <li className='white'>02-02-2022 <strong>#4134534 </strong>  <a href=" ">Se ordre</a> <a href="">Genbestil</a></li>
+            <li>02-02-2022 <strong>#4134534 </strong>  <a href=" ">Se ordre</a> <a href="">Genbestil</a></li>
+            <li className='white'>02-02-2022 <strong>#4134534 </strong>  <a href=" ">Se ordre</a> <a href="">Genbestil</a></li>
+            <li>02-02-2022 <strong>#4134534 </strong>  <a href=" ">Se ordre</a> <a href="">Genbestil</a></li>
+            <li className='white'>02-02-2022 <strong>#4134534 </strong>  <a href=" ">Se ordre</a> <a href="">Genbestil</a></li>
+          </ul>
+        </div>
+    </div>
   )
 }
 
