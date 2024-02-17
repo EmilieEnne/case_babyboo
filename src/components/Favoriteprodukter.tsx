7886c3b28cc879f3fb81006956c6bb234fda0e1f
EmilieEnne <130250704+EmilieEnne@users.noreply.github.com>
2024-02-17 15:52:18 +0100

    Tilføjet useState

--- src/components/Favoriteprodukter.tsx
+++ src/components/Favoriteprodukter.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import Hands from '../Assets/hands-heart 1.png'
+import Ordreliste from '../Assets/Frame 63.png'
+import { useState } from 'react'
+import './Favoritprodukter.css'
+import ProduktBillede from '../Assets/image 7.png'
+import Bin from '../Assets/Frame 63 (1).png'
 
-type Props = {}
+const Favoriteprodukter = () => {
+  const [Edit, setEdit] = useState(false);
 
-const Favoriteprodukter = (props: Props) => {
+  const handleClick = () => {
+    setEdit(prevState => !prevState);
+  }
   return (
-    <div>Favoriteprodukter</div>
+    <div className='fav'>
+     <img src={Hands} alt="Favorit vector" />
+        <h2>FAVORITPRODUKTER</h2>
+        <span><a href="#" onClick={handleClick}>Redigér</a></span>  
+        <div className="favoritecard">
+          <div className="fav-produkt">
+            <img src={ProduktBillede} alt="" />
+            <span className='fav-navn'>DYLAN PUSLEPUDE</span>
+            <div className="float-right">
+            <img src={Edit ? Bin : Ordreliste} alt={Edit ? "Skraldespand vector" : "Ordreliste vector"} />
+            </div>
+          </div>
+          <div className="fav-produkt">
+            <img src={ProduktBillede} alt="" />
+            <span className='fav-navn'>DYLAN PUSLEPUDE</span>
+            <div className="float-right">
+              <img src={Edit ? Bin : Ordreliste} alt={Edit ? "Skraldespand vector" : "Ordreliste vector"} />
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
